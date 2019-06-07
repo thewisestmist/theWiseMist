2ff63ac37f6941113bdf00ff97d27df5e4046e68
derekmurr <derekmurr@gmail.com>
2019-06-07 14:30:53 -0400

    fixed mist image filepaths

--- src/components/WinModal.js
+++ src/components/WinModal.js
@@ -1,15 +1,14 @@
 import React from "react";
+import MistTile from "./MistTile.js";
 
 const WinModal = (props) => {
   return (
     <div className="winModal">
-      <h2>CONGRATION! YOU DONE IT</h2>
-      <div className="mistContainer">
-        <img src="./assets/wiseMist.png" className="wiseMist mist1" alt="The Wise Mist" />
-        <img src="./assets/wiseMist.png" className="wiseMist mist2" alt="The Wise Mist" />
-        <img src="./assets/wiseMist.png" className="wiseMist mist3" alt="The Wise Mist" />
+      <h2>Well done!</h2>
+      <div className="mistModalWrap">
+        <MistTile />
       </div>
-      <p>Well done, {props.userName}! Here is your wisdom from The Wise Mist:</p>
+      <p>Congratulations, {props.userName}! Here is your wisdom from The Wise Mist:</p>
       <p>{props.wisdomMessage}</p>
       <button onClick={() => props.resetGame()}>Play again?</button>
     </div>
