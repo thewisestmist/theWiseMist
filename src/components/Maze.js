b55c6db383d273a9e16370ba6d1083c348990228
derekmurr <derekmurr@gmail.com>
2019-06-10 13:04:48 -0400

    commenting and tidying code

--- src/components/Maze.js
+++ src/components/Maze.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Tile from "./Tile.js";
 
+// builds the actual maze array
 class Maze extends React.Component {
   makeMaze = () => {
+    // map through each row of the 2D array
     let displayMaze = this.props.maze.map((tiles, i) => {
+      // map through each column of the 2D array
       return tiles.map((tile, j) => {
+        // spit out a tile object with fill value and x,y placement values
         return <Tile fillValue={this.props.maze[i][j]} gridX={j} gridY={i} key={`${i}-${j}`} />;
       });
     });
